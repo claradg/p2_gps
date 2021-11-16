d97506d09ea638d4a13f19336279515a31794d61
claradg <claradiaz02@hotmail.es>
2021-11-16 19:07:18 +0100

    creacion docker-compose.yml [ci skip]

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-
 while ! curl http://mongo:27017/
 do
-  echo "$(date) - still trying"
-  sleep 1
+    echo "$(date) - still trying"
+    sleep 1
 done
 echo "$(date) - connected successfully"
 
-java -jar ordering-*.jar
+java -jar usr/local/app/ordering-service-0.1-0.jar -spring.config.location=application.properties
+
+
